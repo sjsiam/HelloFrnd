b3d2bc976f0a3a08cf5234e39638243fad3a490f
siam02 <siam.jaman02@gmail.com>
2025-08-27 06:37:52 +0600

    User name shown

--- src/app/messages/page.tsx
+++ src/app/messages/page.tsx
@@ -1,7 +1,10 @@
-export default function MessagesPage() {
+import { getUser } from "@/src/lib/getUser";
+
+export default async function MessagesPage() {
+  const user = await getUser();
   return (
     <div>
-      <h1>Private Messages</h1>
+      <h1>Private Messages - {user?.name}</h1>
       <p>This content is only visible to logged-in users.</p>
     </div>
   );
